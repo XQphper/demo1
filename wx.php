8fbec7a17607be1c7d25f7437a9569bd575bd1c3
Your Name <you@example.com>
2017-06-02 21:35:06 +0800

    项目初始化

---
+++ wx.php
@@ -0,0 +1,107 @@
+<?php
+/**
+  * wechat php test
+  */
+
+//define your token
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+
+if ($_GET['echostr'])
+{
+	$wechatObj->valid();
+}
+else
+{
+	$wechatObj->responseMsg();
+}
+
+$wechatObj->valid();
+
+class wechatCallbackapiTest
+{
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+        	echo $echoStr;
+        	exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		// $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+		// $postStr = file_get_contents('php://input');
+		// file_put_contents('./1.txt', $postStr);
+
+      	//extract post data
+		if (!empty($postStr)){
+                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+                   the best way is to check the validity of xml by yourself */
+                libxml_disable_entity_loader(true);
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>"; 
+				if(!empty( $keyword ))
+                {
+              		$msgType = "text";
+                	$contentStr = "辣茩懂善";
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
+    }
+		
+	private function checkSignature()
+	{
+        /*
+        1㏒???token?⊿timestamp?⊿nonce豕y??2?那y??DD℅?米?D辰??D辰
+        2㏒???豕y??2?那y℅?﹞?∩??∩?車3谷辰???℅?﹞?∩???DDsha1?車?邦
+        3㏒??a﹞⊿????米??車?邦o車米?℅?﹞?∩??谷車?signature??㊣豕㏒?㊣那那??????車角∩?∩車迆?⊿D?
+         */
+        // you must define TOKEN by yourself
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+        
+        $signature = $_GET["signature"];
+        
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+		$token = TOKEN;
+		
+        $tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
