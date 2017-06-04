e78d3e373753ffb4b5ff25eac103de4955379009
Your Name <you@example.com>
2017-06-04 15:33:29 +0800

    写入数据库 4

--- wx.php
+++ wx.php
@@ -35,18 +35,18 @@ class wechatCallbackapiTest
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
-		 $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-//		 $postStr = file_get_contents('php://input');
+//		 $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+		 $postStr = file_get_contents('php://input');
 		 var_dump($postStr);
 		 file_put_contents('./1.txt' , $postStr);
 //		 file_put_contents('./1.txt', json_decode($postStr));
 
-//        include "./db.php";
-//
-//        $data = array(
-//            'xml' =>$postStr,
-//        );
-//        $database ->insert('xml' ,$data);
+        include "./db.php";
+
+        $data = array(
+            'xml' =>$postStr,
+        );
+        $database ->insert('xml' ,$data);
 
       	//extract post data
 		if (!empty($postStr)){
